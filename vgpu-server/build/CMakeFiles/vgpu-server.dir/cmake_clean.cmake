d09b0418d93be7045124096e41fae99d0052f3de
Magnus Olsen <magnus@greatlord.com>
2019-07-22 09:14:35 +0200

    New build system cmake and visual stuido

---
+++ vgpu-server/build/CMakeFiles/vgpu-server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vgpu-server.dir/main.c.o"
+  "CMakeFiles/vgpu-server.dir/vgpu_cmd.c.o"
+  "CMakeFiles/vgpu-server.dir/home/olsen/Documents/Test_Network/codeblocks/dyad/src/dyad.c.o"
+  "vgpu-server.pdb"
+  "vgpu-server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vgpu-server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ vgpu-server/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/olsen/Documents/Test Network/codeblocks/vgpu-server/vgpu-server
+# Build directory: /home/olsen/Documents/Test Network/codeblocks/vgpu-server/vgpu-server/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
